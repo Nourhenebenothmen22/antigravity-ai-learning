cd81bb42e45b29c975cf0ba90c1a9372b31076dc
nourhene <benothmennourhen8@gmail.com>
2025-12-27 18:56:42 +0100

    feat(utils): add generateToken function for JWT creation

---
+++ backend/utils/generateToken.js
@@ -0,0 +1,6 @@
+// utils/generateToken.js
+import jwt from "jsonwebtoken";
+
+export const generateToken = (payload, expiresIn = "1h") => {
+  return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn });
+};
