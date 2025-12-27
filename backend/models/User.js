dec70d05a328eda64883191dbebe5ef7f7d20a70
nourhene <benothmennourhen8@gmail.com>
2025-12-27 18:37:57 +0100

    feat(models): add User model with name, email, password, and profileImage

---
+++ backend/models/User.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+const userSchema=new mongoose.Schema({
+    name:{
+        type:String,
+    },
+    email:{
+        type:String,
+    },
+    password:{
+        type:String,
+    },
+    profileImage:{
+        type:String,
+    }
+})
+export default mongoose.model("User",userSchema)
