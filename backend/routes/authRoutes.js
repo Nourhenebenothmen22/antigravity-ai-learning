e87a4976629819372a281d08eaeac62f316eeeb8
nourhene <benothmennourhen8@gmail.com>
2025-12-27 18:42:40 +0100

    feat(routes): add protected routes for profile and password change with auth middleware

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,10 +1,16 @@
 import express from "express";
 import { registerValidation } from "../middlewares/auth.validation.js";
-import { register, login } from "../controllers/auth.controller.js";
+import { register, login,getProfile,updateProfile,changePassword } from "../controllers/auth.controller.js";
+import authMiddleware from "../middlewares/auth.js";
 const router = express.Router();
 
-
+//public routes
 router.post("/register", registerValidation, register);
 router.post("/login", login);
+
+//protected routes
+router.get("/profile",authMiddleware,getProfile)
+router.put("/profile",authMiddleware,updateProfile)
+router.post("change-password",authMiddleware,changePassword)
 
 export default router;
