c70b0844dc426a8e950f1a5449652da03ba16180
nourhene <benothmennourhen8@gmail.com>
2025-12-28 22:35:32 +0100

    fix: correct change-password route path

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,16 +1,17 @@
 import express from "express";
 import { registerValidation } from "../middlewares/auth.validation.js";
-import { register, login,getProfile,updateProfile,changePassword } from "../controllers/auth.controller.js";
+import { register, login,getProfile,updateProfile,changePassword,logout } from "../controllers/auth.controller.js";
 import authMiddleware from "../middlewares/auth.js";
 const router = express.Router();
 
 //public routes
 router.post("/register", registerValidation, register);
 router.post("/login", login);
+router.post("/logout", logout);
 
 //protected routes
 router.get("/profile",authMiddleware,getProfile)
 router.put("/profile",authMiddleware,updateProfile)
-router.post("change-password",authMiddleware,changePassword)
+router.post("/change-password", authMiddleware, changePassword)
 
 export default router;
