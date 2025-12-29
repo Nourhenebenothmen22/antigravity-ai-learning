c012491cebfb4d479652f10caa8f6eafc8e9450c
nourhene <benothmennourhen8@gmail.com>
2025-12-29 13:53:06 +0100

    feat(auth): implement profile image upload in auth routes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,17 +1,18 @@
 import express from "express";
 import { registerValidation } from "../middlewares/auth.validation.js";
-import { register, login,getProfile,updateProfile,changePassword,logout } from "../controllers/auth.controller.js";
+import { register, login, getProfile, updateProfile, changePassword, logout } from "../controllers/auth.controller.js";
 import authMiddleware from "../middlewares/auth.js";
+import { uploadProfileImage } from "../config/multer.config.js";
 const router = express.Router();
 
 //public routes
-router.post("/register", registerValidation, register);
+router.post("/register", uploadProfileImage.single("profileImage"), registerValidation, register);
 router.post("/login", login);
 router.post("/logout", logout);
 
 //protected routes
-router.get("/profile",authMiddleware,getProfile)
-router.put("/profile",authMiddleware,updateProfile)
-router.post("/change-password", authMiddleware, changePassword)
+router.get("/profile", authMiddleware, getProfile);
+router.put("/profile", authMiddleware, uploadProfileImage.single("profileImage"), updateProfile);
+router.post("/change-password", authMiddleware,changePassword)
 
 export default router;
