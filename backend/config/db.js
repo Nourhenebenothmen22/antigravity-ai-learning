50b42445f708889e1ddaecf6168dd02c372f7316
nourhene <benothmennourhen8@gmail.com>
2025-12-29 13:52:10 +0100

    feat(db): add MongoDB connection with logger and error handling

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,13 @@
+import logger from "../config/logger.js";
 import mongoose from "mongoose";
 
 const connectDB=async()=>{
     try {
         await mongoose.connect(process.env.MONGO_URI);
-        console.log("MongoDB connected");
+       logger.info("MongoDB connected");
     } catch (error) {
-        console.log(error);
+        logger.error(error);
+        process.exit(1);
     }
 }
 
